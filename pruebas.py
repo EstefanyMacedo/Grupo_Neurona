29d3cc33c4574c423b1bc0ac73e23a08098bad0a
jijuarez12 <128627222+jijuarez12@users.noreply.github.com>
2023-07-09 23:10:53 -0500

    Delete pruebas.py

--- pruebas.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-from numpy import arange
-import pandas as pd 
-# %matplotlib widget
-import  matplotlib.pyplot as plt 
-n = np.arange(8)
-t =np.arange(0,8,0.1)
-print(n)
-print(t)
